32145c8bdaca94a4492fc471dd9026c759757ef5
gabrielsiguenza <gebsiguenza@gmail.com>
2018-05-10 13:53:35 -0700

    add Junit Test outline, include examples of prebuilt test from Prof Nery

--- Tests/com/gsiguenza/MainTest.java
+++ Tests/com/gsiguenza/MainTest.java
@@ -1,10 +1,69 @@
 package com.gsiguenza;
 
+/*
+import org.junit.Before;
+import org.junit.Test;
+
+//import necessary packages;
+
+import static org.junit.Assert.*;
+ */
+
+//TODO: Write tests for Main.
 /**
  * Test for main class. In case we want to practice some testing, final project would be the
  * best place to do so.
  */
 public class MainTest {
+    // static final variables for creation of class objects and methods testing.
+
+    // private variable declaration for class instantiation.
+
+    // You may build helper methods in this section, for example you may want a class that instantiates all lists,
+    // hashmaps etc. using the variable constants you created above.
+
+    /**
+     * with the @Before tag, this method will called before EVERY SINGLE @Test method that runs
+     */
+    // @Before
+    // void method setUp() // instantiation of class objects.
+
+    /**
+     * JUnit Tests Section.
+     */
+    //@Test
+    // Various tests can be written in this section.
+
+    /**
+     * EXAMPLE test from KWLinkedListTest
+     *
+     * @author Nery Chapleton-Lamas
+     */
+    /* do not uncomment this block as these will not work here.
+    Use as reference, can be deleted after implementation.
+
+    public void testNewLinkedListBySize() {
+
+    ASSERT TRUE USAGE EXAMPLE:
+        assertTrue("String list should start as empty", stringList.isEmpty());
+        assertTrue("Integer list should start as empty", intList.isEmpty());
+        assertTrue("Character list should start as empty", charList.isEmpty());
+        assertTrue("Double list should start as empty", doubleList.isEmpty());
+
+    ASSERT EQUALS USAGE EXAMPLE:
+        assertEquals("String list expected toString doesn't match actual", TO_STRING_INSERT[0], stringList.toString());
+
+    ASSERT FALSE USAGE EXAMPLE:
+        assertFalse("Expected value NOT to be in String list, but your code claims it is", stringList.contains(STRING_INSERT_VAL));
+
+    FAILED TEST MESSAGE USAGE EXAMPLE:
+        fail("String list get() test should have thrown exception for out of bounds (empty list, index == 0)");
+    }
+    */
+    // END OF EXAMPLE TEST.
+
+    // note in reference to the above test, each test should do ONE thing.
+    // think carefully about the most logical implementation.
 
 
 }
