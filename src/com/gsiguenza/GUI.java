c4d5d4694f3eb6bd901f8e4152240fe58b77e158
fogelm <mikefogelj@gmail.com>
2018-05-12 23:59:33 -0700

    add GUI implementation

--- src/com/gsiguenza/GUI.java
+++ src/com/gsiguenza/GUI.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI {
+    //Buttons
     private JButton buttonOne;
     private JButton buttonTwo;
     private JButton buttonThree;
@@ -18,7 +19,31 @@ public class GUI {
     private JButton buttonEight;
     private JButton buttonNine;
     private JButton buttonTen;
+    //Panels
     private JPanel MAIN_PANEL;
+    private JPanel listPanel;
+    //Labels
+    private JLabel listTitle;
+    private JLabel numOne;
+    private JLabel numTwo;
+    private JLabel numThree;
+    private JLabel numFour;
+    private JLabel numFive;
+    private JLabel numSix;
+    private JLabel numSeven;
+    private JLabel numEight;
+    private JLabel numNine;
+    private JLabel numTen;
+    //Strings for input
+    private String coinChoiceForData;
+    private String coinChoiceForHighChange;
+    private String coinChoiceForLowChange;
+    private String highPriceCoin;
+    private String lowPriceCoin;
+    private String openHigh;
+    private String openLow;
+    private String closeHigh;
+    private String closeLow;
 
     public GUI() {
         buttonOne.addActionListener(new ActionListener() {
@@ -31,63 +56,63 @@ public class GUI {
         buttonTwo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                coinChoiceForData = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view data for: ");
             }
         });
         buttonThree.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                coinChoiceForHighChange = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view highest % change for: ");
             }
         });
         buttonFour.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                coinChoiceForLowChange = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view lowest % change for: ");
             }
         });
         buttonFive.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                highPriceCoin = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view highest prices for: ");
             }
         });
         buttonSix.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                lowPriceCoin = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view lowest prices for: ");
             }
         });
         buttonSeven.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                openHigh = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view highest opening prices for: ");
             }
         });
         buttonEight.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                openLow = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view lowest opening prices for: ");
             }
         });
         buttonNine.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                closeHigh = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view highest closing prices for: ");
             }
         });
         buttonTen.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showInputDialog
+                closeLow = JOptionPane.showInputDialog
                         (null, "Please enter a coin you would like to view lowest closing prices for: ");
             }
         });
